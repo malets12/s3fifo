f64854ea8ab658b876bf35d544214682abcc6b4a
Yehor Malets <malets12@gmail.com>
2024-03-12 20:24:33 +0200

    Benchmark dependencies, concurrent test

---
+++ src/test/java/org/openscience/PerformanceTest.java
@@ -0,0 +1,13 @@
+package org.openscience;
+
+import org.junit.jupiter.api.Test;
+
+public class PerformanceTest {
+
+    @Test
+    void populateParallelOnce() {
+        //TODO https://github.com/cache2k/cache2k-benchmark/blob/master/jmh-suite/src/main/java/org/cache2k/benchmark/jmh/BenchmarkBase.java
+        //TODO https://github.com/cache2k/cache2k-benchmark/blob/master/jmh-suite/src/main/java/org/cache2k/benchmark/jmh/cacheSuite/PopulateParallelOnceBenchmark.java
+
+    }
+}
---
+++ src/test/java/org/openscience/S3FifoCacheConcurrencyTest.java
@@ -0,0 +1,37 @@
+package org.openscience;
+
+import org.junit.jupiter.api.Test;
+import org.openscience.cache.Cache;
+import org.openscience.cache.s3fifo.S3FifoCache;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class S3FifoCacheConcurrencyTest {
+    @Test
+    void shouldBeVisibleToOtherThreads() {
+        Cache<Integer, String> cache = new S3FifoCache<>(100);
+        ExecutorService writers = Executors.newFixedThreadPool(20);
+        ExecutorService readers = Executors.newFixedThreadPool(20);
+
+        AtomicInteger counter = new AtomicInteger();
+        LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(200);
+
+        for (int i = 0; i < 200; i++) {
+            writers.submit(() -> {
+                int c = counter.getAndIncrement();
+                queue.add(c);
+                cache.put(c, String.valueOf(c));
+                assertEquals(String.valueOf(c), cache.get(c)); //touch
+            });
+        }
+        for (int i = 0; i < 200; i++) {
+            readers.submit(() -> {
+                Integer c = queue.poll();
+                assertEquals(String.valueOf(c), cache.get(c));
+            });
+        }
+    }
+}
